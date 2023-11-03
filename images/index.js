fda30b9fedd150dc9c33143fe20f51ed03b147e5
Oseidolivia <oseidolivia@gmail.com>
2023-11-02 21:57:04 -0500

    Module 19 Deliverables

---
+++ images/index.js
@@ -0,0 +1,7 @@
+var Module19 = "Module 19";
+console.log(Module19);
+
+
+$(".button1").on("click", function(){
+    $('.cssChange').css("background-color", "Red");
+});
---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+$('#button3').on("click", function(){
+    $('.cssChange2').css("background-color","#5f2e48");
+  });
+
+$('#button2').on("click", function(){
+    $('.cssChange1').css("background-color","#5f2e48");
+  });
